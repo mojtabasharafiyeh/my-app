6faf389ed12ccfad814f848947511466f0f63fa1
mojtabasharafiyeh <61268429+mojtabasharafiyeh@users.noreply.github.com>
2023-05-04 20:43:18 +0430

    some fix

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
-import ExpenseItem from './component/ExpenseItem.js'
+import React, { createElement } from 'react'
+import Expenses from './component/Expenses'
 function App() {
-  const Expenses = [
+  const Expenses1 = [
     { id: 'm1', title: 'chair', amount: '900', date: new Date() },
     { id: 'm2', title: 'bag', amount: '200', date: new Date(2020, 2, 6) },
     { id: 'm3', title: 'shoe', amount: '1200', date: new Date(2021, 3, 7) },
     { id: 'm4', title: 'pc', amount: '2000', date: new Date(2023, 4, 8) },
   ]
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "let's get started"),
+  //   createElement(Expenses, { prop: Expenses1 }),
+  // )
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={Expenses[0].title}
-        amount={Expenses[0].amount}
-        date={Expenses[0].date}
-      ></ExpenseItem>{' '}
-      <ExpenseItem
-        title={Expenses[1].title}
-        amount={Expenses[1].amount}
-        date={Expenses[1].date}
-      ></ExpenseItem>{' '}
-      <ExpenseItem
-        title={Expenses[2].title}
-        amount={Expenses[2].amount}
-        date={Expenses[2].date}
-      ></ExpenseItem>{' '}
-      <ExpenseItem
-        title={Expenses[3].title}
-        amount={Expenses[3].amount}
-        date={Expenses[3].date}
-      ></ExpenseItem>
+      <Expenses prop={Expenses1} />
     </div>
   )
 }
---
+++ src/component/Expenses.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import ExpenseItem from './ExpenseItem'
+import './Expenses.css'
+import Card from './Card'
+function Expenses(props) {
+  return (
+    <Card className='expenses'>
+      <ExpenseItem
+        title={props.prop[0].title}
+        amount={props.prop[0].amount}
+        date={props.prop[0].date}
+      ></ExpenseItem>{' '}
+      <ExpenseItem
+        title={props.prop[1].title}
+        amount={props.prop[1].amount}
+        date={props.prop[1].date}
+      ></ExpenseItem>{' '}
+      <ExpenseItem
+        title={props.prop[2].title}
+        amount={props.prop[2].amount}
+        date={props.prop[2].date}
+      ></ExpenseItem>{' '}
+      <ExpenseItem
+        title={props.prop[3].title}
+        amount={props.prop[3].amount}
+        date={props.prop[3].date}
+      ></ExpenseItem>
+    </Card>
+  )
+}
+export default Expenses
